d2f97672bfc049becfb29e672356d760781535f5
7max18 <maxkoppel0703@gmail.com>
2023-12-12 20:23:58 -0500

    Completed 2023 Day 8 Part 1

---
+++ 2023/d8p1.py
@@ -0,0 +1,24 @@
+
+
+with open("Input/Day8Input.txt") as f:
+    lines = f.readlines()
+    instructions = lines[0]
+    nodes = dict()
+    for line in lines[2:]:
+        nodes[line[:3]] = (line[7:10], line[12:15])
+
+cur = "AAA"
+pointer = 0
+counter = 0
+while cur != "ZZZ":
+    turn = instructions[pointer]
+    if turn == "L":
+        cur = nodes[cur][0]
+    else:
+        cur = nodes[cur][1]
+    counter += 1
+    pointer += 1
+    if pointer == len(instructions)-1:
+        pointer = 0
+
+print(counter)
