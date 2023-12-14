6e6efb8d6901f0490c9614b64853f6920e71144e
7max18 <maxkoppel0703@gmail.com>
2023-12-14 11:17:18 -0500

    Solved 2023 Day 11 Part 2

--- 2023/d11p2.py
+++ 2023/d11p2.py
@@ -1,8 +1,16 @@
-#Potential optimization: no need to expand the galaxy if each expansion just adds an additional step
+#No need to expand the galaxy if each expansion just adds an additional step
 from itertools import combinations
-
-def manhattan(start, end):
-    return sum(abs(value1 - value2) for value1, value2 in zip(start, end))
+EXPANSION_RATE = 1000000
+def manhattan(start, end, empty_rows, empty_columns):
+    distance = 0
+    empty = [empty_rows, empty_columns]
+    paired_coords = zip(start, end, empty)
+    for value1, value2, empty_values in paired_coords:
+        distance += abs(value1 - value2)
+        for empty_space in empty_values:
+            if min(value1,value2) < empty_space < max(value1,value2):
+                distance += EXPANSION_RATE-1
+    return distance
 
 with open("Input/Day11Input.txt") as f:
     universe = [line.strip() for line in f.readlines()]
@@ -13,15 +21,10 @@ empty_columns = list()
 for i, row in enumerate(universe):
     if "#" not in row:
         empty_rows.append(i)
-for row in empty_rows[::-1]:
-    universe.insert(row, "."*len(universe[0]))
 for i in range(len(universe[0])):
     column = [row[i] for row in universe]
     if "#" not in column:
         empty_columns.append(i)
-for column in empty_columns[::-1]:
-    for row in range(len(universe)):
-        universe[row] = universe[row][:column] + "." + universe[row][column:]
 galaxies = set()
 for row, row_str in enumerate(universe):
     for column, elem in enumerate(row_str):
@@ -30,6 +33,6 @@ for row, row_str in enumerate(universe):
 galaxy_pairs = combinations(galaxies, 2)
 paths = list()
 for pair in galaxy_pairs:
-    shortest_path = manhattan(pair[0],pair[1])
+    shortest_path = manhattan(pair[0],pair[1],empty_rows,empty_columns)
     paths.append(shortest_path)
 print(sum(paths))
