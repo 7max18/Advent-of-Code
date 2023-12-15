6050cbcd93fa29e4989a2504287ad8f6ab335d8f
7max18 <maxkoppel0703@gmail.com>
2023-12-15 09:56:29 -0500

    Solved 2023 Day 12 Part 2

--- 2023/d12p2.py
+++ 2023/d12p2.py
@@ -1,25 +1,46 @@
-# Start with the clues, not the springs.
-
-def get_solutions(row, clues):
-    row = row.split(".")
-    minimums = [list() for x in range(len(row))]
-    for index, segment in enumerate(row):
-        counter = 0
-        for spring in segment:
-            if spring == "#":
-                counter += 1
-            else:
-                if counter != 0:
-                    minimums[index].append(counter)
-                counter = 0
-    print(minimums)
-
-    
+def get_solutions(row, clues, pos=0, clue_pos=0, counter=0):
+    if (pos, clue_pos, counter, row[pos]) in ways:
+        return ways[(pos, clue_pos, counter, row[pos])]
+    clue_index = clue_pos
+    start_count = counter
+    for row_index, condition in list(enumerate(row))[pos:]:
+        if condition == ".":
+            if counter > 0:
+                if counter == clues[clue_index]:
+                    counter = 0
+                    clue_index += 1
+                else:
+                    return 0
+        elif condition == "#":
+            if clue_index == len(clues):
+                ways[(pos, clue_pos, start_count, row[pos])] = 0
+                return 0
+            counter += 1
+            if counter > clues[clue_index]:
+                ways[(pos, clue_pos, start_count, row[pos])] = 0
+                return 0
+        elif condition == "?":
+            operational = get_solutions(row[:row_index]+"."+row[row_index+1:], clues, row_index, clue_index, counter)
+            ways[(row_index, clue_index, counter, ".")] = operational
+            damaged = get_solutions(row[:row_index]+"#"+row[row_index+1:], clues, row_index, clue_index, counter)
+            ways[(row_index, clue_index, counter, "#")] = damaged
+            return damaged + operational
+    else:
+        if counter > 0 and counter == clues[clue_index]:
+            clue_index += 1
+        if clue_index == len(clues):
+            ways[(pos, clue_pos, start_count, row[pos])] = 1
+            return 1
+        else:
+            ways[(pos, clue_pos, start_count, row[pos])] = 0
+            return 0
+ 
 with open("Input/Day12Input.txt") as f:           
     springs = list()
     for line in f.readlines():
-        springs.append(["?".join([line.split()[0] for i in range(5)]), eval("["+",".join([line.split()[1] for i in range(5)])+"]")])
-solutions = list()
+        springs.append(["?".join([line.split()[0] for i in range(5)]), eval("("+",".join([line.split()[1] for i in range(5)])+")")])
+total = 0
 for row, clues in springs:
-    solutions.append(get_solutions(row,clues))
-print(sum(solutions))
+    ways = dict()
+    total += get_solutions(row, clues)
+print(total)
