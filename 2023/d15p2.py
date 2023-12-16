d1016d6fd8b72a018e57743dd2d0563c56487a88
7max18 <maxkoppel0703@gmail.com>
2023-12-16 10:41:17 -0500

    Solved 2023 Day 15 Part 2

--- 2023/d15p2.py
+++ 2023/d15p2.py
@@ -9,7 +9,30 @@ def hash(step):
 with open("Input/Day15Input.txt") as f:
     sequence = f.readline().strip().split(",")
 
-checksum = 0
+boxes = [list() for i in range(256)]
+
 for step in sequence:
-    checksum += hash(step)
-print(checksum)
+    if step[-1].isdigit():
+        label = step[:-2]
+        operation = step[-2]
+        focal_length = int(step[-1])
+    else:
+        label = step[:-1]
+        operation = step[-1]
+    index = hash(label)
+    cur_lens = [lens for lens in boxes[index] if lens[0] == label]
+    if operation == "-":
+        if cur_lens:
+            boxes[index].remove(cur_lens[0])
+    elif operation == "=":
+        if cur_lens:
+            boxes[index] = [(label, focal_length) if lens[0] == label else lens for lens in boxes[index]]
+        else:
+            boxes[index].append((label, focal_length))
+
+focusing_power = 0
+for i, box in enumerate(boxes):
+    for j, lens in enumerate(box):
+        focusing_power += (i+1) * (j+1) * lens[1] 
+
+print(focusing_power)
