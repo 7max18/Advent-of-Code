44cacf6364fa9b02b7ac619b6775f75219214875
7max18 <maxkoppel0703@gmail.com>
2023-12-18 13:20:51 -0500

    Added turtle to 2023 Day 18 Part 2

--- 2023/d18p2.py
+++ 2023/d18p2.py
@@ -8,6 +8,12 @@ with open("Input/Day18Input.txt") as f:
     for line in f.readlines():
         direction = line.split()
         directions.append([int(direction[2][7]), int(direction[2][2:7], 16)])
+
+turtle.tracer(False)
+turt = turtle.Turtle()
+turt.hideturtle
+turt.shapesize(PIXEL_SIZE/20)
+
 border_points = 0
 shoelace = 0
 vertices = [[0,0]]
@@ -16,14 +22,19 @@ for index, direction in enumerate(directions):
     cur = vertices[-1].copy()
     match direction[0]:
         case 3:
+            turt.setheading(90)
             cur[1] += direction[1]
         case 0:
+            turt.setheading(0)
             cur[0] += direction[1]
         case 1:
+            turt.setheading(-90)
             cur[1] -= direction[1]
         case 2:
+            turt.setheading(180)
             cur[0] -= direction[1]
     border_points += direction[1]
+    turt.forward(direction[1]/40000)
     vertices.append(cur)
 shoelace = 0
 for i in range(len(vertices)-1):
@@ -32,3 +43,5 @@ for i in range(len(vertices)-1):
 interior_area = abs(shoelace)//2
 interior_points = abs(shoelace//2) + 1 - (border_points//2)
 print(interior_points + border_points)
+turtle.tracer(True)
+turtle.done()
