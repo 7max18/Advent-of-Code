33d6452580fde7aa59138d62400787bf30a4224d
7max18 <maxkoppel0703@gmail.com>
2023-12-22 21:04:40 -0500

    Solved 2023 Day 22 Part 2

--- 2023/Day22/d22p2.py
+++ 2023/Day22/d22p2.py
@@ -2,7 +2,7 @@
 #This can be determined by creating a directed graph of which bricks are supported by other bricks.
 #A brick is at rest if its lowest point's Z coordinate is 1, or if its Z-coordinate is one greater than a brick at rest.
 #AND its X, Y coordinates overlap with said brick.
-#The bricks need to be sorted from lowest to highest somehow.
+#The bricks need to be sorted from lowest to highest.
 
 import matplotlib.pyplot as plt
 from matplotlib import colors
@@ -23,6 +23,19 @@ from random import random
 #         voxel_coords[start[0]:end[0]+1, start[1]:end[1]+1, start[2]:end[2]+1] = True
 #         b.voxels(voxel_coords, facecolors=color)
 #     plt.show()
+
+def get_chain(matrix, row_num, fallen):
+    row = matrix[row_num]
+    current = fallen.copy()
+    current.add(row_num)
+    for col_num, supporting in enumerate(row):
+        if supporting and not any([row[col_num] for index, row in enumerate(matrix) if index != row_num and index not in fallen]):
+            fallen.add(col_num)
+    next_step = fallen - current
+    if next_step:
+        for brick in next_step:
+            fallen.update(get_chain(matrix, brick, fallen))
+    return fallen
 
 with open("Day22Input.txt") as f:
     bricks = [[eval("["+p+"]")for p in line.split("~")] + [False] for line in f.readlines()]
@@ -46,18 +59,13 @@ while not all([b[2] for b in bricks]):
     for i, brick in [x for x in enumerate(bricks) if not x[1][2]]:
         bricks[i][0][2] -= 1
         bricks[i][1][2] -= 1
+#[print(line) for line in brick_supports]
 
 #Use adjacency matrix to identify which bricks support other bricks once all have fallen
-can_disintegrate = list()
+chain_reactions = list()
 for row_num, supporter in enumerate(brick_supports):
-    for col_num, supporting in enumerate(supporter):
-        if supporting:
-            if not any([row[col_num] for index, row in enumerate(brick_supports) if index != row_num]):
-                break
-    else:
-        can_disintegrate.append(row_num)
-
-print(len(can_disintegrate))
+    chain_reactions.append(len(get_chain(brick_supports, row_num, set())))
+print(sum(chain_reactions))  
 
 #Bonus visulalization in matplotlib
 # all_edge_points = [j for i in [b[:2] for b in bricks] for j in i]
