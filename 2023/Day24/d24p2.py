ac160cdc7d384b21179f2bad0c3479c2f06c5dfd
7max18 <maxkoppel0703@gmail.com>
2023-12-24 22:31:28 -0500

    Minor edit

--- 2023/Day24/d24p2.py
+++ 2023/Day24/d24p2.py
@@ -16,25 +16,6 @@ with open("Day24Input.txt") as f:
 solutions = set()
 hailstone_trios = combinations(hailstones, 3)
 for trio in hailstone_trios:
-    x1 = trio[0][0][0]
-    vx1 = trio[0][1][0]
-    y1 = trio[0][0][1]
-    vy1 = trio[0][1][1]
-    z1 = trio[0][0][2]
-    vz1 = trio[0][1][2]
-    x2 = trio[0][0][0]
-    vx2 = trio[0][1][0]
-    y2 = trio[0][0][1]
-    vy2 = trio[0][1][1]
-    z2 = trio[0][0][2]
-    vz2 = trio[0][1][2]
-    x3 = trio[0][0][0]
-    vx3 = trio[0][1][0]
-    y3 = trio[0][0][1]
-    vy3 = trio[0][1][1]
-    z3 = trio[0][0][2]
-    vz3 = trio[0][1][2]
-
     system = [x+q*(v-trio[0][1][0])-trio[0][0][0],
               y+q*(u-trio[0][1][1])-trio[0][0][1],
               z+q*(w-trio[0][1][2])-trio[0][0][2],
