4a9c4a822c1119c326335ae1331f615ac01e28d3
7max18 <maxkoppel0703@gmail.com>
2024-12-02 13:00:45 -0500

    Solved D1P2

--- 2024/Day1/p2.py
+++ 2024/Day1/p2.py
@@ -7,4 +7,18 @@ with open('input.txt') as f:
         list_a.append(int(line[0]))
         list_b.append(int(line[1]))
 
-for 
+occurrences = list()
+
+for i, a in enumerate(list_a):
+    if i > 0 and a in list_a[:i-1]:
+        occurrences.append(occurrences[list_a.index(a)])
+        continue
+    total = 0
+    for b in list_b:
+        if a == b:
+            total += 1
+    occurrences.append(total)
+
+final = sum([a*b for a,b in zip(list_a, occurrences)])
+
+print(final)
