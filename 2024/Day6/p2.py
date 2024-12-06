e760ef1b9ce6b931638591886dcda955ca55e591
7max18 <maxkoppel0703@gmail.com>
2024-12-06 00:35:05 -0500

    Solved 2024 D6P1

---
+++ 2024/Day6/p2.py
@@ -0,0 +1,25 @@
+with open('input.txt') as f:
+    lab = [line.strip() for line in f.readlines()]
+
+for r, row in enumerate(lab):
+    for c, col in enumerate(row):
+        if col == '^':
+            pos = [r,c]
+
+rots = [(-1,0),(0,1),(1,0),(0,-1)]
+vec = 0
+visited = [pos]
+
+while True:
+    if pos not in visited:
+        visited.append(pos)
+    try:
+        if lab[pos[0]+rots[vec][0]][pos[1]+rots[vec][1]] == '#':
+            vec = (vec + 1) % 4
+        pos = [pos[0]+rots[vec][0],pos[1]+rots[vec][1]]
+    except IndexError:
+        if pos not in visited:
+            visited.append(pos)
+        break
+
+print(len(visited))
